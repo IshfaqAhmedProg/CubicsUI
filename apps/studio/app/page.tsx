c1c082723f3b7077976eec5251151fbbc2dc118b
Ishfaq Ahmed <ishfaqahmed0837@gmail.com>
2025-01-16 07:01:54 +0600

    🐳 chore(studio): Redirected route
    Redirected Home route to Projects by default

--- apps/studio/app/page.tsx
+++ apps/studio/app/page.tsx
@@ -1,25 +1,27 @@
 import { Stack, Typography } from "@mui/material";
 import CreateProjectButton from "./projects/create";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-  return (
-    <Stack
-      alignItems={"center"}
-      gap={4}
-      my="auto"
-    >
-      <Typography
-        color="textSecondary"
-        variant="h4"
-        fontFamily={"var(--font-h)"}
-      >
-        Get started!
-      </Typography>
+  redirect("/projects");
+  // return (
+  //   <Stack
+  //     alignItems={"center"}
+  //     gap={4}
+  //     my="auto"
+  //   >
+  //     <Typography
+  //       color="textSecondary"
+  //       variant="h4"
+  //       fontFamily={"var(--font-h)"}
+  //     >
+  //       Get started!
+  //     </Typography>
 
-      <Typography color="textSecondary">
-        Start your journey by creating your first project
-      </Typography>
-      <CreateProjectButton />
-    </Stack>
-  );
+  //     <Typography color="textSecondary">
+  //       Start your journey by creating your first project
+  //     </Typography>
+  //     <CreateProjectButton />
+  //   </Stack>
+  // );
 }
