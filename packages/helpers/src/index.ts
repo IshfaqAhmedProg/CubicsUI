d2fca4d3715390ed338b45f367d7d0ac5c411471
Ishfaq Ahmed <ishfaqahmed0837@gmail.com>
2025-01-30 07:07:26 +0600

    🎉 init(helpers): Added new package
    Added `helpers` package to contain function used across cubicsui monorepo

---
+++ packages/helpers/src/index.ts
@@ -0,0 +1,12 @@
+export {
+  default as dependencyAnalyser,
+  createExternalDependency,
+  createLocalDependency,
+} from "./functions/dependencyAnalyser.js";
+
+export { default as isNodeModule } from "./functions/isNodeModule.js";
+
+export {
+  resolveLocalDependencyPath,
+  resolveRelativePath,
+} from "./functions/pathResolvers.js";
---
+++ packages/helpers/src/types/TsConfigPaths.ts
@@ -0,0 +1,7 @@
+/**
+ * TypeScript path mapping configuration from tsconfig.json
+ * Example: { "@/*": ["./src/*"] }
+ */
+export type TsConfigPaths = {
+  [key: string]: string[];
+};
